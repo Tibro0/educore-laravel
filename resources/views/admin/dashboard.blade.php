cbaabeeb5712606983e42e892e9f603bf174ed2a
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-12-18 00:00:39 +0600

    Admin Auth System

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,14 +4,14 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>User Dashboard</title>
+    <title>Admin Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-    <h1>User Dashboard</h1>
-    <form method="POST" action="{{ route('logout') }}">
+    <h1>Admin Dashboard</h1>
+    <form method="POST" action="{{ route('admin.logout') }}">
         @csrf
         <a href="" onclick="event.preventDefault(); this.closest('form').submit();" class="btn btn-danger">
             Logout
@@ -24,4 +24,3 @@
 </body>
 
 </html>
-
