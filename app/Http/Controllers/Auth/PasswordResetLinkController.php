6127496c9ab70afadf74ccdc16649ddab6cd7a8d
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-12-19 01:22:44 +0600

    Reset Password All Page Mastering

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -37,8 +37,12 @@ class PasswordResetLinkController extends Controller
         );
 
         return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                        ->withErrors(['email' => __($status)]);
+            ? back()->with('toast', [
+                'type' => 'success',
+                'title' => 'Success',
+                'message' => 'Reset Link Has Been Sent To Your Email.'
+            ])->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
