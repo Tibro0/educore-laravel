2e7449f939e30131e9b0b3a0aa702ddd9ef55a69
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-12-21 16:01:41 +0600

    Course Level Seeder Add

--- database/seeders/CourseLevelSeeder.php
+++ database/seeders/CourseLevelSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\CourseLevel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CourseLevelSeeder extends Seeder
 {
@@ -12,6 +14,25 @@ class CourseLevelSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        CourseLevel::insert([
+            [
+                'name' => 'Expert',
+                'slug' => Str::slug('Expert'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Intermediate',
+                'slug' => Str::slug('Intermediate'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => 'Beginner',
+                'slug' => Str::slug('Beginner'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
