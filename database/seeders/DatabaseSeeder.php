f40a9b3a70638ad5a9a3efbb50eb9000160fc003
MD. Faysal Hossain Tibro <faysaltibro@gmail.com>
2025-12-23 18:14:45 +0600

    Course Migration & Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,6 +28,7 @@ class DatabaseSeeder extends Seeder
             CourseLanguageSeeder::class,
             CourseLevelSeeder::class,
             CourseCategorySeeder::class,
+            CourseSeeder::class,
         ]);
     }
 }
